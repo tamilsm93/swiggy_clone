b34b12576b6cbe56425e1a8777944a554aed7eff
tamilz <tamilcrea@gmail.com>
2023-11-10 07:54:38 +0530

    added so in cart_items_c

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -30,20 +30,11 @@ class CartItemsController < ApplicationController
 
 
 	def twilio_message(message_items, cost)
-		
 		account_sid = Rails.application.credentials[:twilio][:account_sid]
 		auth_token =  Rails.application.credentials[:twilio][:auth_token]
-		@client = Twilio::REST::Client.new(account_sid, auth_token)
-
-		message = message_items.split(',').join(' ')
-		cost    = cost.to_s
-
-		text  =  'The items are' + message_items + ' ' + 'price is'  + ' ' + cost +  'in 10 - 15 mins'
-		message = @client.messages.create(
-			from: '+15737993581',
-			to: '+919080790814',
-			body:  text
-		)
-		puts message.sid
+		binding.pry
+		twilio_service = TwilioMessageService.new(account_sid, auth_token)
+		binding.pry
+		twilio_service.send_message(items, price)
 	end
 end
---
+++ app/service/twilio_message_service.rb
@@ -0,0 +1,27 @@
+require 'twilio-ruby'
+class TwilioMessageService
+
+	def initialize(account_sid, auth_token)
+		binding.pry	
+	  @account_sid = account_sid
+	  @auth_token = auth_token
+	  @client = Twilio::REST::Client.new(@account_sid, @auth_token)
+	 
+	end
+
+
+	def send_message(message_items, cost)
+		
+		message = message_items.split(',').join(' ')
+		cost    = cost.to_s
+		binding.pry
+
+		text  =  'The items are' + message_items + ' ' + 'price is'  + ' ' + cost +  'in 10 - 15 mins'
+		message = @client.messages.create(
+			from: '+15737993581',
+			to: '+919080790814',
+			body:  text
+		)
+		puts message.sid
+	end
+end
